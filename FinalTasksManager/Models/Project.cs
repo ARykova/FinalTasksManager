827fb9b7bcecef387a4658b08324287bd121f86a
Анна Рыкова <anna_rykova@hotmail.com>
2018-04-13 23:13:12 +0700

    added Models.Project and Models.Task

---
+++ FinalTasksManager/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace FinalTasksManager.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public List<Task> Tasks { get; set; }
+
+        public bool IsCompleted
+        {
+            get
+            {
+                return Tasks.All(task => task.IsCompleted);
+            }
+        }
+    }
+}
---
+++ FinalTasksManager/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalTasksManager.Models
+{
+    public enum Priorites { high, normal, low }
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public Priorites Priority { get; set; } = Priorites.normal;
+
+        public DateTime Deadline { get; set; }
+
+        public List<Task> SubTasks { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+    }
+}
